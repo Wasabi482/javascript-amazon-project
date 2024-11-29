dedef1728e3fe875337439ce680214b3d3abd0f9
Wasabi482 <paulbenedicts482@gmail.com>
2024-11-29 09:25:05 +0800

    Updated the cart checkout quantity of amazon.js and checkout.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart, addToCart} from '../data/cart.js';
+import {cart, addToCart, calculateCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -26,7 +26,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-price">
-        $${formatCurrency(product.priceCents / 100)}
+        $${formatCurrency(product.priceCents)}
       </div>
 
       <div class="product-quantity-container">
@@ -62,15 +62,17 @@ products.forEach((product) => {
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 function updateCartQuantity() {
-  let cartQuantity = 0;
+  const cartQuantity = calculateCartQuantity();
 
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
-
-  document.querySelector('.js-cart-quantity')
-    .innerHTML = cartQuantity;
+  if (!cartQuantity) {
+    document.querySelector('.js-cart-quantity').innerHTML = ''; 
+  } 
+  else { 
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity; 
+  }
 }
+updateCartQuantity();
+
 
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart} from "../data/cart.js";
+import {calculateCartQuantity, cart, removeFromCart} from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -95,6 +95,15 @@ cart.forEach((cartItem) => {
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 
+function updateCartQuantity() {
+  const cartQuantity = calculateCartQuantity();
+
+  document.querySelector('.js-return-to-home-link')
+    .innerHTML = `${cartQuantity} items`;
+}
+
+updateCartQuantity();
+
 document.querySelectorAll('.js-delete-link').forEach((link) => {
   link.addEventListener('click', () => {
     const productId = link.dataset.productId;
@@ -103,5 +112,6 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
     const container = document.querySelector(`.js-cart-item-container-${productId}`);
 
     container.remove();
+    updateCartQuantity();
   });
 });
