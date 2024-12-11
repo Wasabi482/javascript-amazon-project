809bf61a8a7fb4deac249114c0b5097431b986c2
Wasabi482 <paulbenedicts482@gmail.com>
2024-12-11 15:24:00 +0800

    Create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import '../data/cart-oop.js'
 //recursion - function re-run itself
 //MVC- updating the data and regenerating all the HTML
 //MVC - Model - View - Controller
