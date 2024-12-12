04b1b0430ad1d736d0e09e7ff442e5486f8b3ad3
Wasabi482 <paulbenedicts482@gmail.com>
2024-12-12 09:19:29 +0800

    Create class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import '../data/cart-oop.js'
+import '../data/cart-class.js'
 //recursion - function re-run itself
 //MVC- updating the data and regenerating all the HTML
 //MVC - Model - View - Controller
